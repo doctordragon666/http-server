4f5f12ffd34177175cbefb7497a9a4e679136d8a
doctordragon666 <945330194@qq.com>
2024-12-31 10:31:33 +0800

    chore: 更新.gitignore和html文档

--- html_docs/index.js
+++ html_docs/index.js
@@ -1,4 +1,4 @@
-// 写一个javascript函数，插入一个超链接指向网址https://github.com/doctordragon666
+
 function insertLink() {
     var link = document.createElement("a");
     link.href = "https://github.com/doctordragon666";
